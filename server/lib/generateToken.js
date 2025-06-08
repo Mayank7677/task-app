b8481ca5b2134f56661f723f5cd87fecd930ebdf
Mayank Panwar <mayankpanwar066@gmail.com>
2025-06-08 13:29:57 +0530

    fixed jwt error

--- server/lib/generateToken.js
+++ server/lib/generateToken.js
@@ -4,7 +4,11 @@ module.exports = async (email, res) => {
   try {
     const token = jwt.sign({ email }, process.env.JWT_SECRET);
 
-    res.cookie("token", token);
+    res.cookie("token", jwtToken, {
+      httpOnly: true,
+      secure: true, // only over HTTPS
+      sameSite: "None", // because frontend and backend are on different domains
+    });
   } catch (error) {
     console.log("error in generateToken : ", error);
   }
